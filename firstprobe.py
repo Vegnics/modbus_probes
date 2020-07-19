bea2cb621b4ef389d3cf71814371073548f64101
Vegnics <pe_linareso@hotmail.com>
2020-07-19 14:41:40 -0500

    Update firstprobe.py

--- firstprobe.py
+++ firstprobe.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3.6
+"""
+EJEMPLO DE COMUNICACIÓN MODBUSTCP BIDIRECCIONAL
+Pymodbus server on port 5020
+Pydmodbus client on port 5021
+
+BLOQUES:
+di: Discrete Inputs
+co: Coil Outputs
+hr: Holding Registers
+ir: Input Registers
+"""
+
 import tkinter as tk
 from pymodbus.client.sync import ModbusTcpClient
 from pymodbus.server.sync import ModbusTcpServer,StartTcpServer
@@ -5,14 +18,39 @@ from pymodbus.datastore import ModbusSequentialDataBlock as dataServer
 from pymodbus.datastore import ModbusSlaveContext as contextSlave
 from pymodbus.datastore import ModbusServerContext as contextServer
 from time import sleep
-myData=dataServer.create() #Creamos el bloque de datos del servidor Modbus TCP
-myData.setValues(0x15,[15000,62000])# Seteamos dos bloques: el bloque 0x15 a 15000 y 0x16 62000
-store=contextSlave(myData)
-context=contextServer(slaves=store)
-StartTcpServer(context,address=("192.168.1.107",5020))
+from threading import Timer
+
+class readHoldRegisters():
+    def __init__(self,client,address,numReg):
+        self.t=t
+        self.client=client
+        self.address=address
+        self.numReg=numReg
+        self.thread = Timer(self.t,self.printregisters)
+    def printregisters(self):
+        response=self.client.read_holding_registers(self.address,self.numReg,unit=0x1)
+        print(response.registers)
+    def start(self):
+        self.thread.start()
+    def cancel(self):
+        self.thread.cancel()
+
+#Asignamos valores para conexión TCP/IP
+serverIp="192.168.1.107" #La IP de esta PC
+serverPort=5020          #El puerto para la escucha
+clientIp="192.168.1.100" #La IP de la otra PC
+clientPort=5021          #El puerto para la lectura/escritura
+
+#CONFIGURAMOS EL CLIENTE MODBUS TCP
+client=ModbusTcpClient(clientIp,clientPort)
+print(client.connect())
+readReg=readHoldRegisters(client,4014,2)
+
+#CONFIGURAMOS EL SERVIDOR MODBUS TCP
+myBlock=dataServer(4015,[1000,8000])                  #Creamos el bloque de datos del servidor Modbus TCP
+store=contextSlave(di=None,co=None,hr=myBlock,ir=None)#Asignamos los bloques de datos al servidor Modbus TCP
+context=contextServer(slaves=store,single=True)
+StartTcpServer(context,address=(serverIp,serverPort)) #Encendemos el servidor ModbusTCP
 while True:
     print("SERVER ACTIVE")
     sleep(5)
-#client = ModbusClient('localhost', port=5020)
-#client.connect()
-#client.read...
