598fe1e997ef6bfc5c4598b6a251155017d05ec7
Vegnics <pe_linareso@hotmail.com>
2020-07-19 11:49:12 -0500

    Create firstprobe.py

---
+++ firstprobe.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.server.sync import ModbusTcpServer,StartTcpServer
+from pymodbus.datastore import ModbusSequentialDataBlock as dataServer
+from pymodbus.datastore import ModbusSlaveContext as contextSlave
+from pymodbus.datastore import ModbusServerContext as contextServer
+from time import sleep
+myData=dataServer.create() #Creamos el bloque de datos del servidor Modbus TCP
+myData.setValues(0x15,[15000,62000])# Seteamos dos bloques: el bloque 0x15 a 15000 y 0x16 62000
+store=contextSlave(myData)
+context=contextServer(slaves=store)
+StartTcpServer(context,address=("192.168.1.107",5020))
+while True:
+    print("SERVER ACTIVE")
+    sleep(5)
